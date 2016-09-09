ca516632f88abcabc27c7890e374e9e2e4279418
Christopher Madan <chris.madan@gmail.com>
2016-09-08 22:31:08 -0700

    frames?

--- assets/frame_parent.js
+++ assets/frame_parent.js
@@ -1,1 +1,10 @@
-console.log($('#view3d').contents().find("#brainbrowser"))
+$(function() {
+$('#view3d').contents().find("#brainbrowser").click(function(event) {
+  $("#pick-x").html($('#view3d').contents().find('#pick-x').text());
+  $("#pick-y").html($('#view3d').contents().find('#pick-y').text());
+  $("#pick-z").html($('#view3d').contents().find('#pick-z').text());
+  $("#pick-index").html($('#view3d').contents().find('#pick-index').text());
+  $("#pick-value").html($('#view3d').contents().find('#pick-value').text());
+});
+
+});
