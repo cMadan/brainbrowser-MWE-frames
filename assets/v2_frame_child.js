315e7a47d6098b1cac20d3094cc81ddf03b8d2d7
Christopher Madan <chris.madan@gmail.com>
2016-09-09 01:13:23 -0700

    dev

--- assets/v2_frame_child.js
+++ assets/v2_frame_child.js
@@ -139,7 +139,7 @@ function handleBrainz(viewer) {
 
 // load multi models
 f=0;
-for (f=0; f<modelUrl.length; f++) {
+//for (f=0; f<modelUrl.length; f++) {
   console.log(f);
   // Load a model into the scene.
     viewer.loadModelFromURL(modelUrl[f], {
@@ -153,22 +153,22 @@ for (f=0; f<modelUrl.length; f++) {
       });
     }
   });
-};
+//};
 
-// f=1;
-// //for (f=0; f<modelUrl.length; f++) {
-//   console.log(f);
-//   // Load a model into the scene.
-//   viewer.loadModelFromURL(modelUrl[f], {
-//     format: modelFormat,
+f=1;
+  console.log(f);
+  // Load a model into the scene.
+    viewer.loadModelFromURL(modelUrl[f], {
+    format: modelFormat,
 
-//     complete: function(){
-//       viewer.loadIntensityDataFromURL(overlayUrl[f], {
-//         format: overlayFormat,
-//         name: "thick2"
-//       });
-//     }
-//   });
+    complete: function(){
+      viewer.loadIntensityDataFromURL(overlayUrl[f], {
+        format: overlayFormat,
+        name: overlayFname[f],
+        model_name: modelFname[f]
+      });
+    }
+  });
 
   // CRM re-adding pick functionality
   function pick(x,y,paint) {
