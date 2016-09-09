a5de369e498f03616a15f94eb7c3018a0378cdc0
Christopher Madan <chris.madan@gmail.com>
2016-09-08 22:33:38 -0700

    fix?

--- assets/frame_parent.js
+++ assets/frame_parent.js
@@ -1,10 +1,10 @@
-$(function() {
-$('#view3d').contents().find("#brainbrowser").click(function(event) {
-  $("#pick-x").html($('#view3d').contents().find('#pick-x').text());
-  $("#pick-y").html($('#view3d').contents().find('#pick-y').text());
-  $("#pick-z").html($('#view3d').contents().find('#pick-z').text());
-  $("#pick-index").html($('#view3d').contents().find('#pick-index').text());
-  $("#pick-value").html($('#view3d').contents().find('#pick-value').text());
-});
-
+ $( window ).load(function() 
+ 	// http://stackoverflow.com/questions/16781758/my-script-only-working-from-console
+ 	$('#view3d').contents().find("#brainbrowser").click(function(event) {
+	  	$("#pick-x").html($('#view3d').contents().find('#pick-x').text());
+	  	$("#pick-y").html($('#view3d').contents().find('#pick-y').text());
+	  	$("#pick-z").html($('#view3d').contents().find('#pick-z').text());
+	  	$("#pick-index").html($('#view3d').contents().find('#pick-index').text());
+	  	$("#pick-value").html($('#view3d').contents().find('#pick-value').text());
+	});
 });
