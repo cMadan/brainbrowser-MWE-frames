17b11f69f646210bab857f81cf127c7ac7b95aa3
Christopher Madan <chris.madan@gmail.com>
2016-09-09 00:20:47 -0700

    dev

--- assets/v2_frame_child.js
+++ assets/v2_frame_child.js
@@ -139,6 +139,7 @@ f=0;
     complete: function(){
       viewer.loadIntensityDataFromURL(overlayUrl[f], {
         format: overlayFormat,
+        name: "thick1"
       });
     }
   });
@@ -153,6 +154,7 @@ f=1;
     complete: function(){
       viewer.loadIntensityDataFromURL(overlayUrl[f], {
         format: overlayFormat,
+        name: "thick2"
       });
     }
   });
