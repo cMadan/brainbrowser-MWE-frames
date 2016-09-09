84b1403c98e18022b3062af28623c1141d6d75fd
Christopher Madan <chris.madan@gmail.com>
2016-09-09 01:25:27 -0700

    dev

--- assets/v2_frame_child.js
+++ assets/v2_frame_child.js
@@ -143,23 +143,28 @@ f=0;
   // Load a model into the scene.
   viewer.loadModelFromURL(modelUrl[f], {
     format: modelFormat
-  });
+  complete: function() {
   viewer.loadIntensityDataFromURL(overlayUrl[f], {
     format: overlayFormat,
     name: overlayFname[f],
     model_name: modelFname[f]
+  });
+  }
   });
 //};
 
 f=1;
   // Load a model into the scene.
   viewer.loadModelFromURL(modelUrl[f], {
-    format: modelFormat
-  });
+    format: modelFormat,
+  complete: function() {
   viewer.loadIntensityDataFromURL(overlayUrl[f], {
     format: overlayFormat,
     name: overlayFname[f],
     model_name: modelFname[f]
+  });
+  }
+
   });
 
   // CRM re-adding pick functionality
