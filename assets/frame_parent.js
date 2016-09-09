0e7478d92471ac91a8c4c69c930beb31546e4ee3
Christopher Madan <chris.madan@gmail.com>
2016-09-08 22:21:46 -0700

    debug

--- assets/frame_parent.js
+++ assets/frame_parent.js
@@ -1,13 +1,1 @@
-$('#view3d').onreadystatechange = function() {
-	    if ( $('#view3d').readyState == 'complete' ) {
-
-$('#view3d').contents().find("#brainbrowser").click(function(event) {
-  $("#pick-x").html($('#view3d').contents().find('#pick-x').text());
-  $("#pick-y").html($('#view3d').contents().find('#pick-y').text());
-  $("#pick-z").html($('#view3d').contents().find('#pick-z').text());
-  $("#pick-index").html($('#view3d').contents().find('#pick-index').text());
-  $("#pick-value").html($('#view3d').contents().find('#pick-value').text());
-});
-
-	    }
-	}
+console.log($('#view3d'))
